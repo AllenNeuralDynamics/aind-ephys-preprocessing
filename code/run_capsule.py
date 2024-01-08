39f19dc4de00776f9ae7ee2e3085c2de1653bb43
Alessio Buccino <alejoe9187@gmail.com>
2024-01-08 10:36:12 +0100

    Update Dockerfile and script

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -5,11 +5,13 @@ warnings.filterwarnings("ignore")
 # GENERAL IMPORTS
 import os
 
+# this is needed to limit the number of scipy threads 
+# and let spikeinterface handle parallelization
 os.environ["OPENBLAS_NUM_THREADS"] = "1"
 
+import argparse
 import numpy as np
 from pathlib import Path
-import shutil
 import json
 import sys
 import time
@@ -23,13 +25,13 @@ import spikeinterface.preprocessing as spre
 from spikeinterface.core.core_tools import check_json
 
 # AIND
-from aind_data_schema.processing import DataProcess
+from aind_data_schema.core.processing import DataProcess
 
 URL = "https://github.com/AllenNeuralDynamics/aind-ephys-preprocessing"
 VERSION = "0.1.0"
 
 preprocessing_params = dict(
-    preprocessing_strategy="cmr",  # 'destripe' or 'cmr'
+    denoising_strategy="cmr",  # 'destripe' or 'cmr'
     min_preprocessing_duration=120,  # if less than this duration, processing is skipped (probably a test recording)
     highpass_filter=dict(freq_min=300.0, margin_ms=5.0),
     phase_shift=dict(margin_ms=100.0),
@@ -43,7 +45,7 @@ preprocessing_params = dict(
     ),
     remove_out_channels=True,
     remove_bad_channels=True,
-    max_bad_channel_fraction_to_remove=0.5,
+    max_bad_channel_fraction=0.5,  # above this fraction, processing is skipped
     common_reference=dict(reference="global", operator="median"),
     highpass_spatial_filter=dict(
         n_channel_pad=60,
@@ -64,50 +66,108 @@ preprocessing_params = dict(
 n_jobs_co = os.getenv("CO_CPUS")
 n_jobs = int(n_jobs_co) if n_jobs_co is not None else -1
 
-job_kwargs = {"n_jobs": n_jobs, "chunk_duration": "1s", "progress_bar": True}
+job_kwargs = {"n_jobs": n_jobs, "chunk_duration": "1s", "progress_bar": False}
 
 data_folder = Path("../data/")
 results_folder = Path("../results/")
 
 
+# filter and resample LFP
+lfp_filter_kwargs = dict(freq_min=0.1, freq_max=500)
+lfp_sampling_rate = 2500
+
+# default event line from open ephys
+data_folder = Path("../data/")
+scratch_folder = Path("../scratch/")
+results_folder = Path("../results/")
+
+si.set_global_job_kwargs(**job_kwargs)
+
+
+parser = argparse.ArgumentParser(description="Preprocess AIND Neurpixels data")
+
+debug_group = parser.add_mutually_exclusive_group()
+debug_help = "Whether to run in DEBUG mode"
+debug_group.add_argument("--debug", action="store_true", help=debug_help)
+debug_group.add_argument("static_debug", nargs="?", default="false", help=debug_help)
+
+
+# positional arguments
+denoising_group = parser.add_mutually_exclusive_group()
+denoising_help = "Which denoising strategy to use. Can be 'cmr' or 'destripe'"
+denoising_group.add_argument("--denoising", choices=["cmr", "destripe"], help=denoising_help)
+denoising_group.add_argument("static_denoising", nargs="?", default="cmr", help=denoising_help)
+
+remove_out_channels_group = parser.add_mutually_exclusive_group()
+remove_out_channels_help = "Whether to remove out channels"
+remove_out_channels_group.add_argument("--no-remove-out-channels", action="store_true", help=remove_out_channels_help)
+remove_out_channels_group.add_argument(
+    "static_remove_out_channels", nargs="?", default="true", help=remove_out_channels_help
+)
+
+remove_bad_channels_group = parser.add_mutually_exclusive_group()
+remove_bad_channels_help = "Whether to remove bad channels"
+remove_bad_channels_group.add_argument("--no-remove-bad-channels", action="store_true", help=remove_bad_channels_help)
+remove_bad_channels_group.add_argument(
+    "static_remove_bad_channels", nargs="?", default="true", help=remove_bad_channels_help
+)
+
+max_bad_channel_fraction_group = parser.add_mutually_exclusive_group()
+max_bad_channel_fraction_help = (
+    "Maximum fraction of bad channels to remove. If more than this fraction, processing is skipped"
+)
+max_bad_channel_fraction_group.add_argument(
+    "--max-bad-channel-fraction", default=0.5, help=max_bad_channel_fraction_help
+)
+max_bad_channel_fraction_group.add_argument(
+    "static_max_bad_channel_fraction", nargs="?", default="0.5", help=max_bad_channel_fraction_help
+)
+
+motion_correction_group = parser.add_mutually_exclusive_group()
+motion_correction_help = "How to deal with motion correction. Can be 'skip', 'compute', or 'apply'"
+motion_correction_group.add_argument("--motion", choices=["skip", "compute", "apply"], help=motion_correction_help)
+motion_correction_group.add_argument("static_motion", nargs="?", default="compute", help=motion_correction_help)
+
+debug_duration_group = parser.add_mutually_exclusive_group()
+debug_duration_help = (
+    "Duration of clipped recording in debug mode. Default is 30 seconds. Only used if debug is enabled"
+)
+debug_duration_group.add_argument("--debug-duration", default=30, help=debug_duration_help)
+debug_duration_group.add_argument("static_debug_duration", nargs="?", default="30", help=debug_duration_help)
+
+
 if __name__ == "__main__":
-    if len(sys.argv) == 5:
-        PREPROCESSING_STRATEGY = sys.argv[1]
-        if sys.argv[2] == "true":
-            DEBUG = True
-            DURATION_S = float(sys.argv[3])
-        else:
-            DEBUG = False
-            DURATION_S = None
-        if sys.argv[4] == "skip":
-            COMPUTE_MOTION = False
-            APPLY_MOTION = False
-        elif sys.argv[4] == "estimate":
-            COMPUTE_MOTION = True
-            APPLY_MOTION = False
-        else:
-            COMPUTE_MOTION = True
-            APPLY_MOTION = True
-    else:
-        PREPROCESSING_STRATEGY = "cmr"
-        DEBUG = False
-        DURATION_S = None
-        COMPUTE_MOTION = True
-        APPLY_MOTION = False
+    args = parser.parse_args()
+
+    DEBUG = args.debug or args.static_debug == "true"
+    DENOISING_STRATEGY = args.denoising or args.static_denoising
+    REMOVE_OUT_CHANNELS = False if args.no_remove_out_channels else args.static_remove_out_channels == "true"
+    REMOVE_BAD_CHANNELS = False if args.no_remove_bad_channels else args.static_remove_bad_channels == "true"
+    MAX_BAD_CHANNEL_FRACTION = float(args.max_bad_channel_fraction or args.static_max_bad_channel_fraction)
+    motion_arg = args.motion or args.static_motion
+    COMPUTE_MOTION = True if motion_arg != "skip" else False
+    APPLY_MOTION = True if motion_arg == "apply" else False
+    DEBUG_DURATION = float(args.debug_duration or args.static_debug_duration)
 
     data_process_prefix = "data_process_preprocessing"
 
+    print(f"Running preprocessing with the following parameters:")
+    print(f"\tDENOISING_STRATEGY: {DENOISING_STRATEGY}")
+    print(f"\tREMOVE_OUT_CHANNELS: {REMOVE_OUT_CHANNELS}")
+    print(f"\tREMOVE_BAD_CHANNELS: {REMOVE_BAD_CHANNELS}")
+    print(f"\tMAX BAD CHANNEL FRACTION: {MAX_BAD_CHANNEL_FRACTION}")
+    print(f"\tCOMPUTE_MOTION: {COMPUTE_MOTION}")
+    print(f"\tAPPLY_MOTION: {APPLY_MOTION}")
+
     if DEBUG:
-        print(f"DEBUG ENABLED - Only running with {DURATION_S} seconds")
+        print(f"\nDEBUG ENABLED - Only running with {DEBUG_DURATION} seconds\n")
 
     si.set_global_job_kwargs(**job_kwargs)
 
-    assert PREPROCESSING_STRATEGY in [
-        "cmr",
-        "destripe",
-    ], f"Preprocessing strategy can be 'cmr' or 'destripe'. {PREPROCESSING_STRATEGY} not supported."
-    preprocessing_params["preprocessing_strategy"] = PREPROCESSING_STRATEGY
-
+    preprocessing_params["denoising_strategy"] = DENOISING_STRATEGY
+    preprocessing_params["remove_out_channels"] = REMOVE_OUT_CHANNELS
+    preprocessing_params["remove_bad_channels"] = REMOVE_BAD_CHANNELS
+    preprocessing_params["max_bad_channel_fraction"] = MAX_BAD_CHANNEL_FRACTION
     preprocessing_params["motion_correction"]["compute"] = COMPUTE_MOTION
     preprocessing_params["motion_correction"]["apply"] = APPLY_MOTION
 
@@ -120,7 +180,6 @@ if __name__ == "__main__":
         print("\n\nPREPROCESSING")
         t_preprocessing_start_all = time.perf_counter()
         preprocessing_vizualization_data = {}
-        print(f"Preprocessing strategy: {PREPROCESSING_STRATEGY}")
 
         for job_config_file in job_config_json_files:
             datetime_start_preproc = datetime.now()
@@ -171,7 +230,7 @@ if __name__ == "__main__":
                 for segment_index in range(recording.get_num_segments()):
                     recording_one = si.split_recording(recording)[segment_index]
                     recording_one = recording_one.frame_slice(
-                        start_frame=0, end_frame=int(DURATION_S * recording.sampling_frequency)
+                        start_frame=0, end_frame=int(DEBUG_DURATION * recording.sampling_frequency)
                     )
                     recording_list.append(recording_one)
                 recording = si.append_recordings(recording_list)
@@ -182,19 +241,22 @@ if __name__ == "__main__":
             print(f"Preprocessing recording: {recording_name}")
             print(f"\tDuration: {np.round(recording.get_total_duration(), 2)} s")
 
-            preprocessing_vizualization_data[recording_name]["timeseries"]["full"] = dict(raw=recording)
+            preprocessing_vizualization_data[recording_name]["timeseries"] = dict()
+            preprocessing_vizualization_data[recording_name]["timeseries"]["full"] = dict(
+                raw=recording.to_dict(relative_to=data_folder, recursive=True)
+            )
             # maybe a recording is from a different source and it doesn't need to be phase shifted
             if "inter_sample_shift" in recording.get_property_keys():
                 recording_ps_full = spre.phase_shift(recording, **preprocessing_params["phase_shift"])
                 preprocessing_vizualization_data[recording_name]["timeseries"]["full"].update(
-                    dict(phase_shift=recording_ps_full)
+                    dict(phase_shift=recording_ps_full.to_dict(relative_to=data_folder, recursive=True))
                 )
             else:
                 recording_ps_full = recording
 
             recording_hp_full = spre.highpass_filter(recording_ps_full, **preprocessing_params["highpass_filter"])
             preprocessing_vizualization_data[recording_name]["timeseries"]["full"].update(
-                dict(highpass=recording_hp_full)
+                dict(highpass=recording_hp_full.to_dict(relative_to=data_folder, recursive=True))
             )
 
             if recording.get_total_duration() < preprocessing_params["min_preprocessing_duration"] and not DEBUG:
@@ -222,10 +284,10 @@ if __name__ == "__main__":
                 all_bad_channel_ids = np.concatenate((dead_channel_ids, noise_channel_ids, out_channel_ids))
 
                 skip_processing = False
-                max_bad_channel_fraction_to_remove = preprocessing_params["max_bad_channel_fraction_to_remove"]
-                if len(all_bad_channel_ids) >= int(max_bad_channel_fraction_to_remove * recording.get_num_channels()):
+                max_bad_channel_fraction = preprocessing_params["max_bad_channel_fraction"]
+                if len(all_bad_channel_ids) >= int(max_bad_channel_fraction * recording.get_num_channels()):
                     print(
-                        f"\tMore than {max_bad_channel_fraction_to_remove * 100}% bad channels ({len(all_bad_channel_ids)}). "
+                        f"\tMore than {max_bad_channel_fraction * 100}% bad channels ({len(all_bad_channel_ids)}). "
                         f"Skipping further processing for this recording."
                     )
                     preprocessing_notes += (
@@ -255,14 +317,14 @@ if __name__ == "__main__":
                         highpass_spatial=recording_hp_spatial.to_dict(relative_to=data_folder, recursive=True),
                     )
 
-                    preproc_strategy = preprocessing_params["preprocessing_strategy"]
-                    if preproc_strategy == "cmr":
+                    denoising_strategy = preprocessing_params["denoising_strategy"]
+                    if denoising_strategy == "cmr":
                         recording_processed = recording_processed_cmr
                     else:
                         recording_processed = recording_hp_spatial
 
                     if preprocessing_params["remove_bad_channels"]:
-                        print(f"\tRemoving {len(bad_channel_ids)} channels after {preproc_strategy} preprocessing")
+                        print(f"\tRemoving {len(bad_channel_ids)} channels after {denoising_strategy} preprocessing")
                         recording_processed = recording_processed.remove_channels(bad_channel_ids)
                         preprocessing_notes += f"\n- Removed {len(bad_channel_ids)} bad channels after preprocessing.\n"
 
@@ -304,7 +366,7 @@ if __name__ == "__main__":
             )
             preprocessing_process = DataProcess(
                 name="Ephys preprocessing",
-                version=VERSION,  # either release or git commit
+                software_version=VERSION,  # either release or git commit
                 start_date_time=datetime_start_preproc,
                 end_date_time=datetime_start_preproc + timedelta(seconds=np.floor(elapsed_time_preprocessing)),
                 input_location=str(data_folder),
@@ -315,7 +377,7 @@ if __name__ == "__main__":
                 notes=preprocessing_notes,
             )
             with open(preprocessing_output_process_json, "w") as f:
-                f.write(preprocessing_process.json(indent=3))
+                f.write(preprocessing_process.model_dump_json(indent=3))
 
         t_preprocessing_end_all = time.perf_counter()
         elapsed_time_preprocessing_all = np.round(t_preprocessing_end_all - t_preprocessing_start_all, 2)
